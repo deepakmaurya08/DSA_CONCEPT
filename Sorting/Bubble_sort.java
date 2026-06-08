2584abc5481f6e66f221c3db51bf881114ca2425
deepakmaurya08 <deepakmaurya0804@gmail.com>
2026-06-08 21:10:27 +0530

    updated

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -1,8 +1,7 @@
 public class Bubble_sort {
     public static void main (String [] args){
        
-        // arr = {3,4,2,1,5,6,8}//
-        
+        // arr = {3,4,2,1,5,6,8}
 
         int arr[] = {3,4,2,1,5,6,8};
         System.out.println("the Original array is : ");
